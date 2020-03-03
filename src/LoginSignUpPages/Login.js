b9241eacb3bc0c6fa12d99a83b3f6cf596afe4c4
abdulrrehman <abdulrrehman45@gmail.com>
2020-03-03 11:36:26 +0500

    pull ashar

--- src/LoginSignUpPages/Login.js
+++ src/LoginSignUpPages/Login.js
@@ -89,7 +89,7 @@ class LogIn extends Component {
 
       
       <TopNavigation/>
-      <div className="container">
+      {/* <div className="container">
         
         {this.state.errorMessage && (
           <div style={{ "width": "30%", "margin": "25px auto" }} className="alert alert-warning" role="alert">
@@ -150,7 +150,67 @@ class LogIn extends Component {
           </form>
 
         </div>
+      </div> */}
+
+
+    <link href="node_modules/@coreui/icons/css/coreui-icons.min.css" rel="stylesheet"/>
+    <link href="node_modules/flag-icon-css/css/flag-icon.min.css" rel="stylesheet"/>
+    <link href="node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="node_modules/simple-line-icons/css/simple-line-icons.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+    <body className="app flex-row align-items-center">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card-group">
+            <div className="card p-4">
+              <div className="card-body">
+                <h1>Login</h1>
+                <p className="text-muted">Sign In to your account</p>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fa fa-user"></i>
+                    </span>
+                  </div>
+                  <input className="form-control" type="text" placeholder="Username"/>
+                </div>
+                <div className="input-group mb-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </div>
+                  <input className="form-control" type="password" placeholder="Password"/>
+                </div>
+                <div className="row">
+                  <div className="col-6">
+                    <button className="btn btn-primary px-4" type="button">Login</button>
+                  </div>
+                  <div className="col-6 text-right">
+                    <button className="btn btn-link px-0" type="button">Forgot password?</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card text-white bg-primary py-5 d-md-down-none">
+              <div className="card-body text-center">
+                <div>
+                  <h2>Sign up</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <button className="btn btn-primary active mt-3" type="button">Register Now!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+    
+    
+  </body>
+
       </div>
     );
   }
--- src/LoginSignUpPages/Signup.js
+++ src/LoginSignUpPages/Signup.js
@@ -93,7 +93,18 @@ class Signup extends Component {
 
       
       <TopNavigation/>
-      <div className="container">
+
+
+      <link href="node_modules/@coreui/icons/css/coreui-icons.min.css" rel="stylesheet"/>
+      <link href="node_modules/flag-icon-css/css/flag-icon.min.css" rel="stylesheet"/>
+      <link href="node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+      <link href="node_modules/simple-line-icons/css/simple-line-icons.css" rel="stylesheet"/>
+      <link href="css/style.css" rel="stylesheet"/>
+      <link href="vendors/pace-progress/css/pace.min.css" rel="stylesheet"/>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+    
+      {/* <div className="container">
         
         {this.state.errorMessage && (
           <div style={{ "width": "30%", "margin": "25px auto" }} className="alert alert-warning" role="alert">
@@ -166,7 +177,75 @@ class Signup extends Component {
           
 
         </div>
+      </div> */}
+    <div className ="app flex-row align-items-center">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card mx-4">
+            <div className="card-body p-4">
+              <h1>Register</h1>
+              <p className="text-muted">Create your account</p>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fa fa-user"></i>
+                  </span>
+                </div>
+                <input className="form-control" type="text" placeholder="Username"/>
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fa fa-envelope"></i>
+                  </span>
+                    
+                </div>
+                <input className="form-control" type="text" placeholder="Email"/>
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fa fa-lock"></i>
+                  </span>
+                </div>
+                <input className="form-control" type="password" placeholder="Password"/>
+              </div>
+              <div className="input-group mb-4">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fa fa-lock"></i>
+                  </span>
+                </div>
+                <input className="form-control" type="password" placeholder="Repeat password"/>
+              </div>
+              <button className="btn btn-block btn-success" type="button">Create Account</button>
+            </div>
+            <div className="card-footer p-4">
+              <div className="row">
+                <div className="col-6">
+                  <button className="btn btn-block btn-facebook" type="button">
+                    <span>facebook</span>
+                  </button>
+                </div>
+                <div className="col-6">
+                  <button className="btn btn-block btn-twitter" type="button">
+                    <span>twitter</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+
+  </div>
+
+
+
+
+
       </div>
     );
   }
