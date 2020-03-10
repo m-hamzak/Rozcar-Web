90c10eafbaf19de0fe034e8a0619cfd7303326b6
ashar312 <ashar.ashfaq2@gmail.com>
2020-03-10 15:56:27 +0500

    commit....

--- src/components/CMS/cms.js
+++ src/components/CMS/cms.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import firebase from "firebase/app";
 import { Modal } from 'react-bootstrap';
+import './cms.css'
 
 class CMS extends Component{
     constructor(){
@@ -11,12 +12,31 @@ class CMS extends Component{
 
     render(){
         return (
-            <div>
+            <div className="contentheader">
                 Content Management System
                 <label> - Image Slider Changes</label>
-                <label> - Who we are? Changes</label>
-                <label> - Why Rozcar Content Changes</label>
+                <label> - Who we are?</label>
+                <input type="text" placeholder="Text Change"></input>
+                <label>Why Buy it?</label>
+                <input type="text" placeholder="Text Change"></input>
+                <label>Why Rozcar ?</label>
+                <select type="select" className="form-control" 
+                            defaultValue="Why Rozcar Tabs" >
+                                <option value="timesaving">Time Saving</option>
+                                <option value="Economical">Economical</option>
+                                <option value="safenserve">Safe and Serve</option>
+                                <option value="inapphazard">In App Hazard</option>
+                                <option value="airconditiona;">Air Condotional</option>
+                                <option value="nofeevacation">No Fee for Vacation</option>
+                                <option value="nopeak">No Peak/Suge</option>
+                                <option value="automatedmatch">Automated Matching process</option>
+                                <option value="effectivecommunication">Effective Communication</option>
+                                <option value="regularannoucment">Regular Announcement</option>
+                                <option value="erm">ERM</option>
+                                <option value="easypayments">Easy Payments</option>
+                            </select>
                 <label> - Package Management Changes</label>
+
             </div>
         )
     }
